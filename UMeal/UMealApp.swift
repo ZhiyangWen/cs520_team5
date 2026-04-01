bd169988faabc54921fa7fb5f781493adf0f4fa3
ZhiyangWen <144734971+ZhiyangWen@users.noreply.github.com>
2026-04-01 13:02:04 -0400

    Initial UMeal project setup with LoginView

---
+++ UMeal/UMealApp.swift
@@ -0,0 +1,17 @@
+//
+//  UMealApp.swift
+//  UMeal
+//
+//  Created by Zhiyang Wen on 3/31/26.
+//
+
+import SwiftUI
+
+@main
+struct UMealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
