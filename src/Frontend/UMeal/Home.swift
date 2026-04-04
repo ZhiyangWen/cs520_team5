64220229ffe368c7952d9880d294aebf9a663942
Shriya Gautam <57238679+bits-and-bytes85@users.noreply.github.com>
2026-04-04 16:31:19 -0400

    added home page

---
+++ src/Frontend/UMeal/Home.swift
@@ -0,0 +1,25 @@
+//
+//  Home.swift
+//  
+//
+//  Created by Shriya Gautam on 4/4/26.
+//
+
+
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        VStack{
+            Header()
+            
+            
+            
+        }
+    }
+}
+
+#Preview {
+    Home()
+}
